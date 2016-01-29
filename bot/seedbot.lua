2a6e9b58b5af56708d9a39e5109a4c33852481e5
LION <12mohammad.fa@gmail.com>
2016-01-29 21:23:56 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -237,7 +237,7 @@ function create_config( )
     "block",
     "antitag"
     },
-    sudo_users = {37194141,174665816,175531746,0,tonumber(our_id)},--Sudo users
+    sudo_users = {163402568,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[The Anti Spammer Zeus V2
